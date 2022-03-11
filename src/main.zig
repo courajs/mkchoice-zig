7f8733762b8dac6205467931e5554d539a0a72d4
Aaron Sikes <aaron@sikes.io>
2022-03-11 10:21:36 -0500

    Read and write from /dev/tty

--- src/main.zig
+++ src/main.zig
@@ -3,10 +3,12 @@ const mibu = @import("mibu");
 const color = mibu.color;
 
 pub fn main() anyerror!void {
-    const stdout = std.io.getStdOut();
-    try stdout.writer().print("{s}Hi{s}", .{ color.fg(.red), color.reset });
-}
+    const tty = try std.fs.openFileAbsolute("/dev/tty", .{ .read = true, .write = true });
+    std.debug.assert(tty.isTty());
+    try tty.writer().print("{s}Hi{s}\n", .{ color.fg(.red), color.reset });
+    var raw = try mibu.term.RawTerm.enableRawMode(tty.handle, .blocking);
+    defer raw.disableRawMode() catch {};
 
-test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
+    var ev = try mibu.events.next(tty);
+    try tty.writer().print("Key event '{s}'\n", .{ev});
 }
