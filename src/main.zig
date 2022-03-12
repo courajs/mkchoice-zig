11525f597ab4ad85c711f8321e5c01760cd6b660
Aaron Sikes <aaron@sikes.io>
2022-03-12 18:28:08 -0500

    Read events properly from the tty

--- src/main.zig
+++ src/main.zig
@@ -11,10 +11,102 @@ pub fn main() anyerror!void {
     var alloc = arena.allocator();
     _ = alloc;
 
-    var args = try read_args(alloc);
-    std.log.err("{}", .{args});
+    // var args = try read_args(alloc);
+    // const args = Args{
+    //     .opts = Options{},
+    //     .choices = &[_][]const u8{ "a", "b", "c" },
+    // };
+    // var selected = try present_choice(args);
+    // std.log.err("{s}", .{selected});
 
-    // try do_term_stuff();
+    try do_term_stuff();
+    // try research_reads();
+}
+
+// todo:
+// https://docs.rs/termion/latest/src/termion/input.rs.html#185-187
+
+fn do_term_stuff() !void {
+    const tty = try std.fs.openFileAbsolute("/dev/tty", .{ .read = true, .write = true });
+    var raw = try mibu.term.RawTerm.enableRawMode(tty.handle, .blocking);
+    defer raw.disableRawMode() catch {};
+
+    var buf = std.fifo.LinearFifo(u8, .{ .Static = 512 }).init();
+    var reader = tty.reader();
+    std.log.err("Waiting for first input\r", .{});
+    var count = try reader.read(buf.writableSlice(0));
+    buf.update(count);
+
+    while (true) {
+        switch (try mibu.events.next(buf.readableSlice(0))) {
+            .none => {
+                buf.realign();
+                count = try reader.read(buf.writableSlice(0));
+                buf.update(count);
+            },
+            .not_supported => {
+                std.log.err("Not supported byte sequence: {any}\r", .{buf.readableSlice(0)});
+                buf.discard(buf.count);
+            },
+            .incomplete => {
+                buf.realign();
+                count = try reader.read(buf.writableSlice(0));
+                buf.update(count);
+            },
+            .event => |ev| {
+                buf.discard(ev.bytes_read);
+                std.log.err("Event ({} bytes): {}\r", .{ ev.bytes_read, ev.event });
+            },
+        }
+    }
+}
+
+fn research_reads() !void {
+    var tty = try std.fs.openFileAbsolute("/dev/tty", .{ .read = true, .write = true });
+    var raw = try mibu.term.RawTerm.enableRawMode(tty.handle, .blocking);
+    defer raw.disableRawMode() catch {};
+
+    var buf: [20]u8 = undefined;
+
+    while (true) {
+        std.log.err("\r\nBlocking read...\r\n", .{});
+        var bytes = try tty.read(&buf);
+        std.log.err("\r\nGot {} bytes: {any}\r\n", .{ bytes, buf[0..bytes] });
+        std.log.err("\r\nsleeping...\r\n", .{});
+        std.time.sleep(std.time.ns_per_s * 2);
+        std.log.err("\r\npost-sleep read...\r\n", .{});
+        bytes = try tty.read(&buf);
+        std.log.err("\r\nGot {} bytes: {any}\r\n", .{ bytes, buf[0..bytes] });
+    }
+}
+
+fn present_choice(args: Args) !?[]const u8 {
+    _ = args;
+
+    var tty = try std.fs.openFileAbsolute("/dev/tty", .{ .read = true, .write = true });
+    var raw = try mibu.term.RawTerm.enableRawMode(tty.handle, .blocking);
+    defer raw.disableRawMode() catch {};
+
+    while (mibu.events.next(tty)) |ev| {
+        switch (ev) {
+            .key => |key| {
+                switch (key) {
+                    .ctrlC, .escape => {
+                        return null;
+                    },
+                    .ctrlM => { // Enter/return
+                        return "chose";
+                    },
+                    else => {
+                        std.log.err("key event: {}\r", .{ev});
+                    },
+                }
+            },
+            else => std.log.err("unsupported event\r", .{}),
+        }
+    } else |_| {}
+
+    return null;
 }
 
 // Caller owns the memory
@@ -49,17 +141,6 @@ fn read_args(alloc: std.mem.Allocator) !Args {
         .opts = args.options,
         .choices = choices,
     };
-}
-
-fn do_term_stuff() !void {
-    const tty = try std.fs.openFileAbsolute("/dev/tty", .{ .read = true, .write = true });
-    std.debug.assert(tty.isTty());
-    try tty.writer().print("{s}Hi{s}\n", .{ mibu.color.fg(.red), mibu.color.reset });
-    var raw = try mibu.term.RawTerm.enableRawMode(tty.handle, .blocking);
-    defer raw.disableRawMode() catch {};
-
-    var ev = try mibu.events.next(tty);
-    try tty.writer().print("Key event '{s}'\n", .{ev});
 }
 
 const Options = struct {
