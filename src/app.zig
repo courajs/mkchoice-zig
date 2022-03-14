8cc039e261ac07d7ffad727212f234bfa89f6065
Aaron Sikes <aaron@sikes.io>
2022-03-14 11:22:36 -0400

    Add help text

--- src/app.zig
+++ src/app.zig
@@ -91,7 +91,54 @@ pub const App = struct {
         }
     }
 
+    pub const help_text =
+        \\Usage: mkchoice [-h|--help] [-v|--vanish]
+        \\            [-p|--prompt <prompt>] [-s|--selection <selection>]
+        \\            [-n|--index <selected index>]
+        \\            [args] [-- <args>]
+        \\
+        \\  mkchoice prompts the user's tty to choose one of the given choices,
+        \\  and outputs the chosen one. Pass - as one of the args to also read
+        \\  line-separated options from stdin. Arguments after -- are taken as
+        \\  literal choices, not interpreted as flags. If you pass no arguments,
+        \\  mkchoice will read from stdin by default.
+        \\
+        \\  You can pass the initially selected value with --selection, which
+        \\  accepts the text of an option that will appear in the list. It
+        \\  defaults to the first item if the specified one can't be found. Or,
+        \\  you can pass a zero-based index with --index.
+        \\
+        \\  Change the selected option with up/down or j/k, and confirm your
+        \\  selection with space or enter.
+        \\
+        \\  If the --vanish flag is given, the prompt will be erased from the
+        \\  terminal before the output is shown. Otherwise, the final state of
+        \\  the prompt will still be visible on the screen.
+        \\
+        \\  Example:
+        \\
+        \\  $ seq 3 | mkchoice -s main a - b -p "Which one?" -- -p -h - -- main z >some-file
+        \\  Which one?
+        \\    a
+        \\    1
+        \\    2
+        \\    3
+        \\    b
+        \\    -p
+        \\    -h
+        \\    -
+        \\    --
+        \\  > main
+        \\    z
+        \\  $ cat some-file
+        \\  main
+    ;
+
     pub fn run(params: Parameters) !u8 {
+        if (params.opts.help) {
+            try std.io.getStdOut().writeAll(help_text);
+            return 0;
+        }
         var app = App{
             .params = params,
             .state = initial_state(params),
